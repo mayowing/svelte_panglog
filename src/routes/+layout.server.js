06ca18aea68f227448fa16e089c1c2149e3ec681
jcpang <mayowing@gmail.com>
2023-09-09 13:14:40 +0900

    타이틀변경

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,7 @@
 export const load = () => {
     return { 
         user : {
-            name: '뉴진스냐옹이',
+            name: '빵냐옹이',
             email: 'cateye@nj.com',
             avatar: 'https://api.dicebear.com/6.x/adventurer/svg?send=Ginger'
         }
